f0cb3f2704c54e6b606d3f13ded560f40a960a72
rkamit45 <rohitkumaramit24@gmail.com>
2026-08-16 06:50:12 +0530

    Updating buildspec.yml file

--- src/main/java/com/example/springbootaws/Sevvice/BookService.java
+++ src/main/java/com/example/springbootaws/Sevvice/BookService.java
@@ -9,7 +9,14 @@ import java.util.List;
 
 @Service
 public class BookService {
-    List<Book> books = new ArrayList<>();
+    List<Book> books = List.of(
+            new Book(101,"core java","Jems",250),
+            new Book(102,"Servlet","Jean",200.0),
+            new Book(101,"spring","rohit",400),
+            new Book(101,"springboot","Ramesh",230),
+            new Book(101,"AWS","Surya",500)
+
+    );
 
     public List<Book> getBooks(){
         return books.stream().sorted(Comparator.comparingDouble(Book::getPrice)).toList();
